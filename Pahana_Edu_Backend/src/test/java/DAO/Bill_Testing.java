3c97f1dc708ca4a47d06c4713064bfb16f167a5a
Hiruni16 <c.hiruni.cg@gmail.com>
2025-08-20 18:59:31 +0530

    bill testing update

--- Pahana_Edu_Backend/src/test/java/DAO/Bill_Testing.java
+++ Pahana_Edu_Backend/src/test/java/DAO/Bill_Testing.java
@@ -1,11 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DAO;
 
 import Utils.bills_DAO;
 import classes.bills;
+import classes.customer;
+import classes.item;
+import Utils.customer_DAO;
+import Utils.item_DAO;
+
 import java.util.List;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
@@ -14,36 +15,60 @@ import org.junit.runners.MethodSorters;
 
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Hiruni
- */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Bill_Testing {
-    private static bills_DAO dao;
+
+    private static bills_DAO billDao;
+    private static customer_DAO custDao;
+    private static item_DAO itemDao;
+
     private static Integer testBillId;
+    private static Integer testCustomerId;
+    private static Integer testItemId;
 
     @BeforeClass
     public static void setup() {
-        dao = new bills_DAO();
+        billDao = new bills_DAO();
+        custDao = new customer_DAO();
+        itemDao = new item_DAO();
+
+        // 1. Create test customer
+        customer testCust = new customer();
+        testCust.setAccount_no("T-001");
+        testCust.setName("Test Customer");
+        testCust.setAddress("123, Testing");
+        testCust.setTele_no(790281077);
+
+        boolean custAdded = custDao.addCustomer(testCust);
+        assertTrue("Test customer should be added", custAdded);
+        testCustomerId = custDao.getAllCustomers().get(custDao.getAllCustomers().size() - 1).getCustomer_id();
+
+        // 2. Create test item
+        item testIt = new item();
+        testIt.setItem_name("Test Item");
+        testIt.setItem_category("Book");
+        testIt.setItem_price(500.0);
+
+        boolean itemAdded = itemDao.addItem(testIt);
+        assertTrue("Test item should be added", itemAdded);
+        testItemId = itemDao.getAllItems().get(itemDao.getAllItems().size() - 1).getItem_id();
     }
 
     // 1. Test Adding a Bill
     @Test
     public void test1_AddBill() {
         bills b = new bills();
-        b.setCustom_id(1);      // Ensure customer with ID=1 exists
-        b.setItem_id(1);        // Ensure item with ID=1 exists
+        b.setCustom_id(testCustomerId);
+        b.setItem_id(testItemId);
         b.setUnits_consumed(5);
         b.setTotal_price(2500.0);
 
-        boolean added = dao.addBill(b);
+        boolean added = billDao.addBill(b);
         assertTrue("Bill should be added successfully", added);
 
-        List<bills> billsList = dao.getAllBills();
+        List<bills> billsList = billDao.getAllBills();
         assertFalse("Bill list should not be empty after insert", billsList.isEmpty());
 
-        // Save the last inserted bill's ID for next tests
         testBillId = billsList.get(billsList.size() - 1).getBill_id();
         assertNotNull("Test bill ID should not be null", testBillId);
     }
@@ -53,7 +78,7 @@ public class Bill_Testing {
     public void test2_GetBillById() {
         assertNotNull("Test bill ID should not be null", testBillId);
 
-        bills b = dao.getBillById(testBillId);
+        bills b = billDao.getBillById(testBillId);
         assertNotNull("Bill should exist after insertion", b);
 
         assertEquals("Units consumed should match", 5, b.getUnits_consumed());
@@ -65,16 +90,16 @@ public class Bill_Testing {
     public void test3_UpdateBill() {
         assertNotNull("Test bill ID should not be null", testBillId);
 
-        bills b = dao.getBillById(testBillId);
+        bills b = billDao.getBillById(testBillId);
         assertNotNull("Bill should exist before update", b);
 
         b.setUnits_consumed(10);
         b.setTotal_price(5000.0);
 
-        boolean updated = dao.updateBill(b);
+        boolean updated = billDao.updateBill(b);
         assertTrue("Bill should be updated successfully", updated);
 
-        bills updatedBill = dao.getBillById(testBillId);
+        bills updatedBill = billDao.getBillById(testBillId);
         assertNotNull("Updated bill should exist", updatedBill);
 
         assertEquals("Updated units consumed should match", 10, updatedBill.getUnits_consumed());
@@ -84,7 +109,7 @@ public class Bill_Testing {
     // 4. Test Getting All Bills
     @Test
     public void test4_GetAllBills() {
-        List<bills> billsList = dao.getAllBills();
+        List<bills> billsList = billDao.getAllBills();
         assertNotNull("Bill list should not be null", billsList);
         assertFalse("Bill list should not be empty", billsList.isEmpty());
     }
@@ -94,10 +119,10 @@ public class Bill_Testing {
     public void test5_DeleteBill() {
         assertNotNull("Test bill ID should not be null", testBillId);
 
-        boolean deleted = dao.deleteBill(testBillId);
+        boolean deleted = billDao.deleteBill(testBillId);
         assertTrue("Bill should be deleted successfully", deleted);
 
-        bills b = dao.getBillById(testBillId);
+        bills b = billDao.getBillById(testBillId);
         assertNull("Deleted bill should not exist anymore", b);
     }
 }
